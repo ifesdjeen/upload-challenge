f0eedab25d5f9eb8c538970f0d4a385a95f47e0e
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2012-05-11 23:03:03 +0200

    Switching back to ec2.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,5 @@
 # Servers
 #
 
-server "linkflow@linkflow1.orceo.internal", :app, :web, :db, :primary => true # IP: 10.2.11.101
-# server "linkflow@ec2-79-125-98-70.eu-west-1.compute.amazonaws.com", :app, :primary => true
+server "linkflow@ec2-79-125-98-70.eu-west-1.compute.amazonaws.com", :app, :primary => true
 
