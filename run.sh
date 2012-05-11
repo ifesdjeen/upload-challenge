799065694d1b4c9b949674c8e7b5c840eeffc7b2
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2012-05-11 20:43:07 +0200

    Fixing run file.

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 UPLOADCHALLENGE_HOME="/a/uploadchallenge/current"
 UPLOADCHALLENGE_JAR="target/uploadchallenge-0.1.0-SNAPSHOT-standalone.jar"
 
-java -Xmx1024m -jar $UPLOADCHALLENGE_HOME/$UPLOADCHALLENGE_JAR --config-file $UPLOADCHALLENGE_HOME/config/production.clj --environment production &
+java -Xmx1024m -jar $UPLOADCHALLENGE_HOME/$UPLOADCHALLENGE_JAR --config $UPLOADCHALLENGE_HOME/config/production.clj --environment production &
 echo $! > $UPLOADCHALLENGE_HOME/tmp/pids/uploadchallenge.pid
 exit 0
 
