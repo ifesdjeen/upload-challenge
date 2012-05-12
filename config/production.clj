1726b8f0ba973e3483305e974d66bdc5c78649b1
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2012-05-12 16:47:52 +0200

    Missing slash in production config.

--- config/production.clj
+++ config/production.clj
@@ -1,7 +1,7 @@
 {
   :storage {
     :tmp-file-path "/applications/uploadchallenge/shared/store"
-    :resource-path "/applications/uploadchallenge/current/src/resources"
+    :resource-path "/applications/uploadchallenge/current/src/resources/"
   }
 
   :http {
